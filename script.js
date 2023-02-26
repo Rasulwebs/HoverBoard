7e0d0f3f87a7fb486d9a3906e68ba7346f45de34
Rasulwebs <boboqulovrasul80@gmail.com>
2023-02-26 20:42:42 +0500

    hover board starter

---
+++ script.js
@@ -0,0 +1,15 @@
+const container = document.getElementById('container')
+const colors = ['#e74c3c', '#8e44ad', '#3498db', '#e67e22', '#2ecc71']
+const SQUARES = 500
+
+for(let i = 0; i < SQUARES; i++) {
+    const square = document.createElement('div')
+    square.classList.add('square')
+
+    square.addEventListener('mouseover', () => setColor(square))
+
+    square.addEventListener('mouseout', () => removeColor(square))
+
+    container.appendChild(square)
+}
+
