7a86a828bb8a409e1c10e7ca910b32ed2a1aeca6
Rasulwebs <boboqulovrasul80@gmail.com>
2023-02-26 20:44:08 +0500

    animation hover board finished

--- script.js
+++ script.js
@@ -13,3 +13,17 @@ for(let i = 0; i < SQUARES; i++) {
     container.appendChild(square)
 }
 
+function setColor(element) {
+    const color = getRandomColor()
+    element.style.background = color
+    element.style.boxShadow = `0 0 2px ${color}, 0 0 10px ${color}`
+ }
+ 
+ function removeColor(element) {
+    element.style.background = '#1d1d1d'
+    element.style.boxShadow = '0 0 2px #000'
+ }
+ 
+ function getRandomColor() {
+     return colors[Math.floor(Math.random() * colors.length)]
+ }
